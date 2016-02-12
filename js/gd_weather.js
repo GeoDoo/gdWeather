3eb8306e8bc2cb0be2187bcb76a4b53714cc9609
Geodoo <geometas@hotmail.com>
2016-02-13 00:45:34 +0200

    Dirty fix of right separator when only two cities

--- js/gd_weather.js
+++ js/gd_weather.js
@@ -1,15 +1,16 @@
 (function ($) {
-  Drupal.behaviors.g_weather = {
-    attach: function (context, settings) {
+		Drupal.behaviors.g_weather = {
+			attach: function (context, settings) {
       // Code to be run on page load, and
       // on ajax load added here 
 			// (test) appID = 7a917428ec6a219b7172ae10f609bd2b
 			var cities = Drupal.settings.g_cities,
-				citiesArr = cities.split(','),
-				appID = Drupal.settings.app_id,
-				weatherURLSingleCity = 'http://api.openweathermap.org/data/2.5/forecast/daily?id=',
-				weatherAPIGroup = "http://api.openweathermap.org/data/2.5/group?id="+Drupal.settings.g_cities+"&APPID="+appID+"&units=metric";
-				
+			citiesArr = cities.split(','),
+			citiesArrLength = citiesArr.length,
+			appID = Drupal.settings.app_id,
+			weatherURLSingleCity = 'http://api.openweathermap.org/data/2.5/forecast/daily?id=',
+			weatherAPIGroup = "http://api.openweathermap.org/data/2.5/group?id="+Drupal.settings.g_cities+"&APPID="+appID+"&units=metric";
+
 			$.getJSON( weatherAPIGroup, {
 				format: "json"
 			})
@@ -33,12 +34,16 @@
 				.done(function( data ) {
 					var numOfAJAXcall = parseInt(getUrlParameter('num', this.url));
 					var cityName = data.city.name,
-						cityTemp = Math.round(data.list[0].temp.day),
-						cityTempIcon = data.list[0].weather[0].icon;
+					cityTemp = Math.round(data.list[0].temp.day),
+					cityTempIcon = data.list[0].weather[0].icon;
 					$('.city-wrapper-'+(numOfAJAXcall+3)+' h5').html(cityName+' '+cityTemp+'&#x2103;');
 					$('.city-wrapper-'+(numOfAJAXcall+3)+' div').html('<img src="http://openweathermap.org/img/w/'+cityTempIcon+'.png">');					
 				});
 
+			}
+
+			if(citiesArrLength == 2) {
+				$('.city-wrapper-1, .city-wrapper-4').addClass('only-two');
 			}
 			
 			$('li.weather-menu-item a').click(function(e){
@@ -50,23 +55,23 @@
 				$('.temp-group-'+index).addClass('active');
 			});
 			
-		
-    }
-  };
+
+		}
+	};
 	
 	function getUrlParameter(sParam, url) {
-    var sPageURL = decodeURIComponent(url),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+		var sPageURL = decodeURIComponent(url),
+		sURLVariables = sPageURL.split('&'),
+		sParameterName,
+		i;
 
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+		for (i = 0; i < sURLVariables.length; i++) {
+			sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
-    }
+			if (sParameterName[0] === sParam) {
+				return sParameterName[1] === undefined ? true : sParameterName[1];
+			}
+		}
 	};
 	
 }(jQuery));
